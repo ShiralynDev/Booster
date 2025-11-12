6285bd866f30baed7dd28deafd5400054af7e4fa
SamC4r <locoporlosrubiks@gmail.com>
2025-11-12 13:04:13 +0100

    buga

--- src/modules/notifications/components/notification-bell.tsx
+++ src/modules/notifications/components/notification-bell.tsx
@@ -64,7 +64,7 @@ export const NotificationBell = () => {
                 <Mail className="size-5" />
                 {!!unreadCount && unreadCount > 0 && (
                 <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs font-bold rounded-full min-w-[18px] h-[18px] flex items-center justify-center px-1">
-                    {unreadCount && unreadCount > 99 ? '99+' : ""}
+                    {unreadCount && unreadCount > 99 ? '99+' : unreadCount}
                 </span>
                 )}
             </button>
